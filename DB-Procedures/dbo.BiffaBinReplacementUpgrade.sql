e003cf11af8b0058ac0f1c3816b6aba8d62c9302
Dutu <421544@portsmouthcc.gov.uk>
2019-11-06 11:01:51 +0000

    Added bin sizes as criteria for display

--- DB-Procedures/dbo.BiffaBinReplacementUpgrade.sql
+++ DB-Procedures/dbo.BiffaBinReplacementUpgrade.sql
@@ -1,7 +1,7 @@
 USE [ProdRubbishRecycling]
 GO
 
-/****** Object:  StoredProcedure [dbo].[BiffaBinReplacementUpgrade]    Script Date: 03/10/2019 14:52:45 ******/
+/****** Object:  StoredProcedure [dbo].[BiffaBinReplacementUpgrade]    Script Date: 06/11/2019 11:00:42 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -17,7 +17,7 @@ GO
 -- =============================================
 CREATE PROCEDURE [dbo].[BiffaBinReplacementUpgrade]
 	-- Add the parameters for the stored procedure here
-
+@BinSize varchar(50)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -25,14 +25,26 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-
+	IF @BinSize is NULL or @BinSize = ''
+	BEGIN
 	SELECT Address as Address, ReasonApplication as reasonApp, BinSize as binSize, Reference as Reference
 	FROM BinReplacementUpgrade
-	WHERE BinDelivered is NULL AND (CustomerQualifies = 'yes' or BinSize != '')
+	WHERE BinDelivered is NULL AND (CustomerQualifies = 'yes' or BinSize != '') 
 	ORDER BY Postcode
-	
+	END
+
+	IF @BinSize != ''
+	BEGIN
+	SELECT Address as Address, ReasonApplication as reasonApp, BinSize as binSize, Reference as Reference
+	FROM BinReplacementUpgrade
+	WHERE BinDelivered is NULL AND (CustomerQualifies = 'yes' or BinSize != '') AND CHARINDEX(BinSize,@BinSize) <> 0
+	ORDER BY Postcode
+	END
+
+
 	
 
 END
+
 GO
 
